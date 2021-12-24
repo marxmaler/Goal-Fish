975c29cb7792a9817df57726eb4800ff34d29f9c
Sabi <84113149+marxmaler@users.noreply.github.com>
2021-12-24 17:52:05 +0900

    style

---
+++ src/client/js/home.js
@@ -0,0 +1,68 @@
+const form = document.querySelector("form");
+const checkboxes = document.querySelectorAll("input[type='checkbox']");
+const progress = document.querySelector("progress");
+const progressPoint = document.querySelector(".progress-point");
+let checkboxCnt = checkboxes.length;
+let checkedCnt = document.querySelectorAll("input[checked]").length;
+const remainingTimeSpan = document.querySelector(".remainingTime");
+const previousDaily = document.querySelector(".previous-daily");
+
+function timeFormat(time){
+    return String(time).padStart(2, "0");
+}
+
+function countDown(){
+    const midNight = new Date();
+    midNight.setHours(0, 0, 0, 0);
+    midNight.setDate(midNight.getDate() + 1);
+    const now = new Date();
+    const timeleft = midNight.getTime() - now.getTime();
+    const hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+    const remaingTime = `${timeFormat(hours)}:${timeFormat(minutes)}:${timeFormat(seconds)}`;
+        
+    remainingTimeSpan.innerText = remaingTime;
+}
+
+window.onload = function() {
+    if(remainingTimeSpan) {
+        countDown();
+    };
+        
+    if(progress){
+    progress.value=checkedCnt/checkboxCnt*100;
+    progressPoint.innerText = `${Math.round(checkedCnt/checkboxCnt*100)}%`;
+    };
+}
+
+function changeOnCheckbox(event){
+    progress.value=checkedCnt/checkboxCnt*100;
+    progressPoint.innerText = `${Math.round(checkedCnt/checkboxCnt*100)}%`;
+    const id = event.target.value;
+    const td = event.target.parentElement;
+    const input = document.createElement("input");
+    input.setAttribute("class","hidden");
+    input.setAttribute("type","text");
+    input.setAttribute("name","changed");
+    input.setAttribute("value", id);
+    td.appendChild(input);
+    form.submit();
+}
+
+function getPreviousDaily(){
+    const date = previousDaily.value;
+    window.location.href=`daily/${date}`;
+}
+
+
+for(let i=0; i<checkboxes.length; i++){
+    checkboxes[i].addEventListener("change", changeOnCheckbox);
+}
+
+previousDaily.addEventListener("change", getPreviousDaily);
+
+if(remainingTimeSpan) {
+    setInterval(countDown, 1000);
+    
+}
--- src/server.js
+++ src/server.js
@@ -22,6 +22,10 @@ app.use(express.urlencoded({extended: true}));
 app.use("/", globalRouter);
 app.use("/daily", dailyRouter);
 
+//static files serving
+app.use("/assets", express.static("assets"));
+
+
 //server에게 특정 포트로 들어오는 request를 listen하라고 명령하는 부분(마지막에 와야 함)
 const handleListening = () => console.log(`Your server is listening to requests on http://localhost:${PORT}`)
 app.listen(PORT, handleListening);
